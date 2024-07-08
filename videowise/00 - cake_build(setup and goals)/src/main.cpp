ed4250c005bc1fb3f2093f9f8b4b5148ce0a7858
Candyrizz <rishavsingh076@gmail.com>
2024-07-08 13:52:10 +0530

    ok
    ok

---
+++ videowise/00 - cake_build(setup and goals)/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout<<"Hello world!"<<std::endl;
+    std::cout<<argv[1];
+    std::cin.get();
+    return 0;
+}
+
+// From Next video i will start the progress in this filesytem 😁😁
